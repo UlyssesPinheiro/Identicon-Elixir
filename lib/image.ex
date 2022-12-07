f40e3ca54641b1057aa4007251e224c2d269c224
SoulMidia <77904013+UlyssesPinheiro@users.noreply.github.com>
2022-12-07 16:38:53 -0300

    Generating and Saving the Final Images

--- lib/image.ex
+++ lib/image.ex
@@ -1,5 +1,5 @@
 defmodule Identicon.Image do
   # This Strut will hold all the data in our app
   # Struts can only hold properties that were created when the strut was defined
-  defstruct hex: nil, color: nil, grid: nil
+  defstruct hex: nil, color: nil, grid: nil, pixel_map: nil
 end
