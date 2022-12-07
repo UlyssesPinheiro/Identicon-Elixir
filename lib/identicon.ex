12128bdb71ea2f42561441cdc49cfd694ae73791
Ulysses Pinheiro <ulyssespinheiro00@gmail.com>
2022-12-07 07:45:45 -0300

    Saving the Strut

---
+++ lib/identicon.ex
@@ -0,0 +1,19 @@
+defmodule Identicon do
+  @moduledoc """
+  Documentation for `Identicon`.
+  """
+
+  @doc """
+
+  """
+  def main(input) do
+    hash_input(input)
+  end
+
+  def hash_input(input) do
+    hex = :crypto.hash(:md5, input)
+    |> :binary.bin_to_list
+
+    %Identicon.Image{hex: hex}
+  end
+end
---
+++ lib/image.ex
@@ -0,0 +1,5 @@
+defmodule Identicon.Image do
+  # This Strut will hold all the data in our app
+  # Struts can only hold properties that were created when the strut was defined
+  defstruct hex: nil
+end
