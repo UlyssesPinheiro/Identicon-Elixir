739ec782a732acc2c1dcd632173cb6d19c59f04f
SoulMidia <77904013+UlyssesPinheiro@users.noreply.github.com>
2022-12-07 14:59:37 -0300

    Build Grid

--- lib/identicon.ex
+++ lib/identicon.ex
@@ -8,6 +8,9 @@ defmodule Identicon do
   """
   def main(input) do
     hash_input(input)
+    |> pick_color()
+    |> build_grid()
+    |> filter_odd_squares()
   end
 
   def hash_input(input) do
@@ -16,4 +19,35 @@ defmodule Identicon do
 
     %Identicon.Image{hex: hex}
   end
+
+  def pick_color(image) do
+    %Identicon.Image{hex: [r, g, b | _tail]} = image
+
+    %Identicon.Image{image | color: {r, g, b}}
+  end
+
+  def build_grid(image) do
+    %Identicon.Image{hex: hex} = image
+
+    grid =
+      Enum.chunk(hex, 3)
+      |> Enum.map(&mirror_row/1)
+      |> List.flatten
+      |> Enum.with_index
+
+    %Identicon.Image{image | grid: grid}
+  end
+
+  def mirror_row(row) do
+  # [145, 46, 200]
+  [first, second | _tail] = row
+
+  # [145, 46, 200, 46, 145]
+  row ++ [second, first ]
+  end
+
+  def filter_odd_squares(image) do
+    %Identicon.Image{grid: grid} = image
+  end
+
 end
--- lib/image.ex
+++ lib/image.ex
@@ -1,5 +1,5 @@
 defmodule Identicon.Image do
   # This Strut will hold all the data in our app
   # Struts can only hold properties that were created when the strut was defined
-  defstruct hex: nil
+  defstruct hex: nil, color: nil, grid: nil
 end
